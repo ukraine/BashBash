37852af59042f1dec6cb1b1ca985757d063420dd
Yuriy <ua@ifstudio.org>
2015-03-10 23:53:58 +0400

    # The script checks partitions and sends alerts if one of them is running out of space # Just change mail@mail.mail to your actual email address, add a task to cron and enjoy!

---
+++ checkDiskSize.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# The script checks partitions and sends alerts if one of them is running out of space
+# Just change mail@mail.mail to your actual email address to have it working.
+
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $6 }' | while read output;
+do
+  echo $output
+  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
+  partition=$(echo $output | awk '{ print $2 }' )
+  if [ $usep -ge 80 ]; then
+    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
+     mail -s "Alert: Almost out of disk space $usep%" mail@mail.mail
+  fi
+done
